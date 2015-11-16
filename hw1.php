f830185c67c7f2a206b068e771ea8516cbb714c4
三浦宏予 <yoooon0908@yahoo.co.jp>
2015-11-16 20:41:34 +0800

    bootstrapを使ってボタンを変える

--- hw1.php
+++ hw1.php
@@ -2,26 +2,31 @@
 <html lang ="ja">
 <head>
 <meta charset="UTF-8">
-<title>HW</title>
+	<link rel="stylesheet" href="/hw/assets/css/bootstrap.css" type="text/css"/> 
+  	<link rel="stylesheet" href="hw/assets/css/main.css" type="text/css">
+   <link rel="stylesheet" href="hw/assets/css/next.css" type="text/css">
+  <title>HW</title>
 </head>
 <body>
 	<!-- formで$POSTに送る→$_POSTから引っ張ってくる -->
-	<form action="" method="post">
-	<input type="text" name="comment" /><br />
-	<input type="submit" value="送信"/>
-	</form>
-	Hello!!
-	<?php
-		if(isset($_POST['comment'])){
-		$comment = $_POST['comment'];
-
-		function hello($comment){
-			return htmlspecialchars($comment);
-		}
-		}
-		echo hello($comment);
-	?>
-	さん
+		<form action="" method="post">
+			<input type="text" name="comment" /><br />
+			<!-- <input type="submit" value="送信"/> -->
+			<button type="submit" class="btn btn-default btn-lg">
+		  	<span class="glyphicon glyphicon-flash" aria-hidden="true"></span> Push
+			</button>
+		</form>
 </body>
 </html>
+
+				<?php
+					error_reporting(0);
+					if(isset($_POST['comment'])){
+					$comment = $_POST['comment'];
+					$hello = "Hello!!" . $comment . "さん";
+					htmlspecialchars($comment);
+					echo $hello;
+					}
+				?>
+				
 
